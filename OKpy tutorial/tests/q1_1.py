dba16a561d1616cf92db2697b293fa7cd1608580
jasonsjiang <jasonjiang@berkeley.edu>
2018-06-07 15:39:20 -0700

    OKpy tutorial

---
+++ OKpy tutorial/tests/q1_1.py
@@ -0,0 +1,32 @@
+test = {
+  'name': 'Question 1_1', 
+  'points': 1, 
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> x == 35
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> x - 35 > 0
+          True
+          >>> x + 35 == 70
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }                         
+  ]
+}
